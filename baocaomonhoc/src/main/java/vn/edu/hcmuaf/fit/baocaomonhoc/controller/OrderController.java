0a1b054bd89cc33e850426f806ac5949316b9ef0
22130056-HoHoangDuy <22130056@st.hcmuaf.edu.vn>
2025-01-12 14:20:13 +0700

    update js

--- baocaomonhoc/src/main/java/vn/edu/hcmuaf/fit/baocaomonhoc/controller/OrderController.java
+++ baocaomonhoc/src/main/java/vn/edu/hcmuaf/fit/baocaomonhoc/controller/OrderController.java
@@ -14,10 +14,10 @@ import java.util.List;
 public class OrderController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        OrderDao orderDao = new OrderDao();
-        Products list = OrderDao.getOrderById();
-        request.setAttribute("o", list);
-        request.getRequestDispatcher("order.jsp").forward(request, response);
+//        OrderDao orderDao = new OrderDao();
+//        Products list = OrderDao.getOrderById();
+//        request.setAttribute("o", list);
+//        request.getRequestDispatcher("order.jsp").forward(request, response);
     }
 
     @Override
--- baocaomonhoc/src/main/java/vn/edu/hcmuaf/fit/baocaomonhoc/dao/OrderDao.java
+++ baocaomonhoc/src/main/java/vn/edu/hcmuaf/fit/baocaomonhoc/dao/OrderDao.java
@@ -10,58 +10,58 @@ import java.util.List;
 
 public class OrderDao {
 
-    public static List<Orders> getAllOrders() {
-        Statement statement = DBConnect.get();
-        ResultSet rs = null;
-        ArrayList<Orders> listOrders = new ArrayList<>();
-        String sql = "select * from orders";
-        try {
-            rs = statement.executeQuery(sql);
-            while (rs.next()) {
-                listOrders.add(new Orders(
-                        rs.getInt(1),
-                        rs.getDouble(2),
-                        rs.getString(3),
-                        rs.getString(4),
-                        rs.getString(5)
-                ));
-            }
-            return listOrders;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return listOrders;
-    }
+//    public static List<Orders> getAllOrders() {
+//        Statement statement = DBConnect.get();
+//        ResultSet rs = null;
+//        ArrayList<Orders> listOrders = new ArrayList<>();
+//        String sql = "select * from orders";
+//        try {
+//            rs = statement.executeQuery(sql);
+//            while (rs.next()) {
+//                listOrders.add(new Orders(
+//                        rs.getInt(1),
+//                        rs.getDouble(2),
+//                        rs.getString(3),
+//                        rs.getString(4),
+//                        rs.getString(5)
+//                ));
+//            }
+//            return listOrders;
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return listOrders;
+//    }
 
-    public static Products getOrderById() {
-        Statement statement = DBConnect.get();
-        ResultSet resultSet = null;
-        String sql = "select * from products where productId=" + 1;
-        try {
-            resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                return new Products(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getDouble(3),
-                        resultSet.getString(4),
-                        resultSet.getString(5),
-                        resultSet.getInt(6)
-                );
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    public static Products getOrderById() {
+//        Statement statement = DBConnect.get();
+//        ResultSet resultSet = null;
+//        String sql = "select * from products where productId=" + 1;
+//        try {
+//            resultSet = statement.executeQuery(sql);
+//            while (resultSet.next()) {
+//                return new Products(
+//                        resultSet.getInt(1),
+//                        resultSet.getString(2),
+//                        resultSet.getDouble(3),
+//                        resultSet.getString(4),
+//                        resultSet.getString(5),
+//                        resultSet.getInt(6)
+//                );
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
-    public static void main(String[] args) {
-        OrderDao dao = new OrderDao();
-        List<Orders> orders = dao.getAllOrders();
-        for (Orders o : orders) {
-            System.out.println(o);
-        }
-    }
+//    public static void main(String[] args) {
+//        OrderDao dao = new OrderDao();
+//        List<Orders> orders = dao.getAllOrders();
+//        for (Orders o : orders) {
+//            System.out.println(o);
+//        }
+//    }
 
 }
 
